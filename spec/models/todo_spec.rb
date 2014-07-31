eb369999f27fa22ac236f3cd8055fd5fe155f129
Devan Beitel <devan.beitel@gmail.com>
2014-07-31 09:10:30 -0500

    Fix model spec test expiration_date

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 describe Todo do
   before do
     @user = create(:user)
-    @todo = create(:todo)
+    @todo = create(:todo, created_at: Time.new(2014, 7, 31, 9, 5, 0))
   end
 
   describe "#expiration_date" do
     it "returns the number of days remaining before todo is deleted" do
-      expect(@todo.expiration_date).to eq(@todo.created_at+7.days)
+      expect(@todo.expiration_date).to eq(Time.new(2014, 8, 7, 9, 5, 0))
     end
   end
 
